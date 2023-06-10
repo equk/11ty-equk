5d76c1ec96ba84e3980efd156d5974c6aefd9368
equk <equk@users.noreply.github.com>
2023-06-10 11:39:55 +0100

    🧹 eslint: lint all the things

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ["standard", "eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['standard', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  ignorePatterns: ["_site/**/*"],
-};
+  ignorePatterns: ['_site/**/*'],
+}
