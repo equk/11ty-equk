458969672b275d1fe8ac3cdb16c720decc2b10aa
equk <equk@users.noreply.github.com>
2023-06-10 11:37:09 +0100

    added eslintrc that extends eslint/standard, eslint/recommended & prettier/recommended

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: ["standard", "eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: ["_site/**/*"],
+};
