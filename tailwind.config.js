dd93ce43588b979085761501709d2c58d44ace59
equk <equk@users.noreply.github.com>
2023-06-20 14:50:54 +0100

    added config files for tailwindcss & postcss

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['src/**/*.{js,md,njk,svg}'],
+  darkMode: 'class',
+  corePlugins: {
+    touchAction: false,
+    ringOffsetWidth: false,
+    ringOffsetColor: false,
+    scrollSnapType: false,
+    borderOpacity: false,
+    textOpacity: false,
+    fontVariantNumeric: false,
+  },
+  theme: {
+    extend: {
+      colors: {
+        bgColor: 'var(--theme-bg)',
+        textColor: 'var(--theme-text)',
+        link: 'var(--theme-link)',
+        accent: 'var(--theme-accent)',
+        'accent-2': 'var(--theme-accent-2)',
+      },
+      fontFamily: {
+        // Add any custom fonts here
+        sans: [...fontFamily.sans],
+        serif: [...fontFamily.serif],
+        mono: [...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+}
