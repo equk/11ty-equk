1496c6c10177b6a192432fc8d5725359c504756d
equk <equk@users.noreply.github.com>
2023-06-20 16:26:57 +0100

    fixed directory for images watchtarget in eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -21,7 +21,7 @@ module.exports = function (eleventyConfig) {
   // https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
   // Watch content images for the image pipeline.
-  eleventyConfig.addWatchTarget('content/**/*.{svg,webp,png,jpeg}')
+  eleventyConfig.addWatchTarget('src/**/*.{svg,webp,png,jpeg}')
 
   // App plugins
   eleventyConfig.addPlugin(pluginDrafts)
