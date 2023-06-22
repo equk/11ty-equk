555ac25e4f586f83afd3f2ee9fb9ddad77962666
equk <equk@users.noreply.github.com>
2023-06-22 15:32:54 +0100

    created darkmode toggle script which uses localstorage & checks for prefers-color-scheme

---
+++ src/_scripts/darkmode.js
@@ -0,0 +1,46 @@
+const root = document.documentElement
+
+function getThemePref() {
+  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+    return localStorage.getItem('theme')
+  }
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+}
+
+const isDark = getThemePref() === 'dark'
+
+const observer = new MutationObserver(() => {
+  const rootIsDark = root.classList.contains('dark')
+
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('theme', rootIsDark ? 'dark' : 'light')
+  }
+})
+observer.observe(root, { attributeFilter: ['class'] })
+
+root.classList.toggle('dark', isDark)
+
+class ToggleDark extends HTMLElement {
+  constructor() {
+    super()
+    const button = this.querySelector('button')
+
+    function setTheme(dark) {
+      document.documentElement.classList.toggle('dark', dark)
+      button.setAttribute('aria-pressed', String(dark))
+      console.log('button pressed')
+    }
+
+    button.addEventListener('click', () => setTheme(!this.isDark()))
+
+    setTheme(this.isDark())
+  }
+
+  isDark() {
+    return document.documentElement.classList.contains('dark')
+  }
+}
+
+customElements.define('toggle-dark', ToggleDark)
