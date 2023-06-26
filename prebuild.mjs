a1628dab2d84290d8b69742d8f2b89d20f85c2d2
equk <equk@users.noreply.github.com>
2023-06-26 19:47:52 +0100

    created prebuild script to make sure all 11ty builds are clean

---
+++ prebuild.mjs
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+/*!
+ * 11ty prebuild script
+ *
+ * https://github.com/equk/
+ *
+ * Copyright (c) 2023 B.Walden. All rights reserved.
+ *
+ * Licensed under the MIT License
+ *
+ * (LICENSE file should be included with script)
+ *
+ */
+import fs from 'fs'
+
+const outputDir = 'dist'
+
+async function preEleventy() {
+  // Start time for cli stats
+  const start = +new Date()
+  console.log('[pre-11ty] Starting Clean Build')
+  // Clean output if exists
+  if (fs.existsSync(outputDir)) {
+    fs.rmSync(outputDir, { recursive: true })
+    console.log(`[pre-11ty] Cleaning Old Build From ${outputDir}`)
+    const end = +new Date()
+    console.log(
+      `[pre-11ty] Build Output: '${outputDir}' Removed (+${end - start}ms)`
+    )
+  } else {
+    console.log(`[pre-11ty] Build Output: '${outputDir}' is clean`)
+  }
+  console.log('\n')
+}
+
+/*
+ * Run Main Function
+ */
+
+preEleventy().catch((error) => {
+  console.error(error)
+  // quit if error
+  process.exit(1)
+})
