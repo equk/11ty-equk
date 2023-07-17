49bc1f83e24940e1e572686acf8b30bb691d09ed
equk <equk@users.noreply.github.com>
2023-07-17 18:23:06 +0100

    added basic netlify plugin for restoring & saving cache on builds

---
+++ netlify/cache-plugin/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async onPreBuild({ utils }) {
+    console.log('Restoring ./_cache/ folder')
+    await utils.cache.restore('./_cache')
+  },
+  async onPostBuild({ utils }) {
+    console.log('Saving ./_cache/ folder for future builds')
+    await utils.cache.save('./_cache')
+  },
+}
