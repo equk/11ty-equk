6a6c31453fff4ad4fac1e3d48c559d95a5708f68
equk <equk@users.noreply.github.com>
2026-03-28 18:58:55 +0000

    add new schema to posts data using zod for basic type checking

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,6 +1,28 @@
 import slugify from 'slugify'
+import z from 'zod'
 
 export default {
+  eleventyDataSchema: function (data) {
+    let schemaTest = z
+      .object({
+        title: z.string(),
+        date: z
+          .string()
+          .or(z.date())
+          .transform((val) => new Date(val)),
+        author: z.string().optional(),
+        draft: z.boolean().nullish(),
+        tags: z.array(z.string()).default([]),
+        image: z.string().nullish(),
+        imgAuthor: z.string().nullish(),
+        imgAuthorURL: z.string().nullish(),
+        mastodonPostId: z.string().nullish(),
+        blueskyPostId: z.string().nullish(),
+        excerpt: z.string().nullish(),
+      })
+      .parse(data)
+    return schemaTest
+  },
   tags: ['posts'],
   layout: 'layouts/post.njk',
   eleventyComputed: {
